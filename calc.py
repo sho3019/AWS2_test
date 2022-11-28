af209e04820f83d00b20117603c35a001acddfbb
sho3019 <daisuke.syou.j8e@densotechno.com>
2022-11-28 13:02:09 +0900

    aa

---
+++ calc.py
@@ -0,0 +1,23 @@
+import sys
+
+sys.path.append("/input")
+import input as i
+
+if(i.error_flag == 0):
+    num1 = i.num1
+    num2 = i.num2
+    operation = i.operation
+
+    ans = 0
+
+    if(operation == "plus"):
+        ans = num1 + num2
+
+    elif(operation == "minus"):
+        ans = num1 - num2
+
+    elif(operation == "cross"):
+        ans = num1 * num2
+
+    elif(operation == "devision"):
+        ans = num1 / num2
---
+++ input.py
@@ -0,0 +1,25 @@
+import sys
+
+args = sys.argv
+
+error_flag = 0
+for i in range(1, len(args) - 1):
+    st = len(args[i])
+    if("0" <= args[i][0] <= "9" or args[i][0] == "-"):
+            pass
+    else:
+        error_flag = 1
+        break
+
+    for n in range(1, st):
+        
+        if("0" <= args[i][n] <= "9"):
+            pass
+        else:
+            error_flag = 1
+            break
+
+if(error_flag == 0):
+    num1 = int(args[1])
+    num2 = int(args[2])
+    operation = args[3]
