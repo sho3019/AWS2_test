1d58dff40661341f8753561308d7b29f2a4c6058
sho3019 <daisuke.syou.j8e@densotechno.com>
2022-11-28 14:15:26 +0900

    oo

--- calc.py
+++ calc.py
@@ -1,30 +1,25 @@
-import sys
+import input
 
-args = sys.argv
 
-error_flag = 0
-for i in range(1, len(args) - 1):
-    st = len(args[i])
-    if("0" <= args[i][0] <= "9" or args[i][0] == "-"):
-            pass
-    else:
-        error_flag = 1
-        break
 
-    for n in range(1, st):
-        
-        if("0" <= args[i][n] <= "9"):
-            pass
-        else:
-            error_flag = 1
-            break
-    
-    if(error_flag == 0):
-        num1 = int(args[1])
-        num2 = int(args[2])
-        operation = args[3]
+ans = 0
+#print(num1)
+if (input.error_flag == 0):
+    if(input.operation == "plus"):
+        ans = input.num1 + input.num2
 
-    #print(type(num1))
+    elif(input.operation == "minus"):
+        ans = input.num1 - input.num2
 
-    else:
-        print("incorrect input")
+    elif(input.operation == "cross"):
+        ans = input.num1 * input.num2
+
+    elif(input.operation == "devision"):
+        ans = input.num1 / input.num2
+
+    #print("answer :" + str(ans))
+
+else:
+    ans = "error: incorrect input"
+
+print("answer :" + str(ans))
