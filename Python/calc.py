c34cc050f555e43df431ec026c97b4ed3502e108
sho3019 <daisuke.syou.j8e@densotechno.com>
2022-11-29 12:49:45 +0900

    AWS2

--- Python/calc.py
+++ Python/calc.py
@@ -1,26 +1,35 @@
 import input
 
-def calc(error_flag, operation, num1, num2):
+def calc(operation, num1, num2):
     ans = 0
 
-    if (error_flag == 0):
-        if(operation == "plus"):
-            ans = num1 + num2
+    if(operation == "plus"):
+        ans = num1 + num2
 
-        elif(operation == "minus"):
-            ans = num1 - num2
+    elif(operation == "minus"):
+        ans = num1 - num2
 
-        elif(operation == "cross"):
-            ans = num1 * num2
+    elif(operation == "cross"):
+        ans = num1 * num2
 
-        elif(operation == "devision"):
-            ans = num1 / num2
+    elif(operation == "devision"):
+        ans = num1 / num2
+    ans = "answer : " + str(ans)
+    
+    return ans
+if(input.error_flag == 0):
+    num1 = input.num1
+    num2 = input.num2
+    error_flag = input.error_flag
+    operation = input.operation
+    ans = calc(operation, num1, num2)
+    print(ans)
+else:
+    print("error: incorrect input")
+#error_flag = 0
+#operation = "plus"
+#num1 = 1
+#num2 = 2
 
-        print("answer :" + str(ans))
-
-    else:
-        ans = "error: incorrect input"
-        print(ans)
-    #return ans
-
-ans = calc(input.error_flag, input.operation, input.num1, input.num2)
+#ans = calc(operation, num1, num2, error_flag)
+#print(ans)
