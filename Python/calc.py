3d9dd159ae7abe05a922b0fe3abc34a0f85a0ab2
sho3019 <daisuke.syou.j8e@densotechno.com>
2022-11-29 09:55:42 +0900

    calc

--- Python/calc.py
+++ Python/calc.py
@@ -1,22 +1,26 @@
 import input
 
-ans = 0
+def calc(error_flag, operation, num1, num2):
+    ans = 0
 
-if (input.error_flag == 0):
-    if(input.operation == "plus"):
-        ans = input.num1 + input.num2
+    if (error_flag == 0):
+        if(operation == "plus"):
+            ans = num1 + num2
 
-    elif(input.operation == "minus"):
-        ans = input.num1 - input.num2
+        elif(operation == "minus"):
+            ans = num1 - num2
 
-    elif(input.operation == "cross"):
-        ans = input.num1 * input.num2
+        elif(operation == "cross"):
+            ans = num1 * num2
 
-    elif(input.operation == "devision"):
-        ans = input.num1 / input.num2
+        elif(operation == "devision"):
+            ans = num1 / num2
 
-    print("answer :" + str(ans))
+        print("answer :" + str(ans))
 
-else:
-    ans = "error: incorrect input"
-    print(ans)
+    else:
+        ans = "error: incorrect input"
+        print(ans)
+    #return ans
+
+ans = calc(input.error_flag, input.operation, input.num1, input.num2)
