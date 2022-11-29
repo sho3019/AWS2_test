691e5b8b8cb5857e18a97c46510c7449d4743319
sho3019 <daisuke.syou.j8e@densotechno.com>
2022-11-29 14:27:09 +0900

    AWS2

--- Python/calc.py
+++ Python/calc.py
@@ -14,22 +14,27 @@ def calc(operation, num1, num2):
 
     elif(operation == "devision"):
         ans = num1 / num2
+    
+    else:
+        #print("error : incorrect operation")
+        pass
+    
     ans = "answer : " + str(ans)
     
     return ans
-if(input.error_flag == 0):
-    num1 = input.num1
-    num2 = input.num2
-    error_flag = input.error_flag
-    operation = input.operation
-    ans = calc(operation, num1, num2)
-    print(ans)
-else:
-    print("error: incorrect input")
+
+
+num1 = input.num1
+num2 = input.num2
+error_flag = input.error_flag
+operation = input.operation
+ans = calc(operation, num1, num2)
+print(ans)
+
 #error_flag = 0
 #operation = "plus"
 #num1 = 1
 #num2 = 2
 
-#ans = calc(operation, num1, num2, error_flag)
+#ans = calc(operation, num1, num2, erroflag)
 #print(ans)
